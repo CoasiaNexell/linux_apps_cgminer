ca04cd261a459abf85d549aa73f91877a62a78db
michelle.lee <michelle.lee@coasianexell.com>
2020-12-02 17:32:55 +0900

    cgminer: Apply cgminer patch related date/time
    This patch applies the patch from 'Source date/time displayed values from
    CLOCK_REALTIME'
    https://github.com/ckolivas/cgminer/pull/735/commits/
    9674ba888ff8c4d3b2e013b51fc18c4ca96d30e1
    
    Signed-off-by: michelle.lee <michelle.lee@coasianexell.com>
    Change-Id: I2bbb3f406cee2a6a0acc5f7e5401bf431eb61d72

--- cgminer.c
+++ cgminer.c
@@ -5246,7 +5246,7 @@ static void set_curblock(const char *hexstr, const unsigned char *bedata)
 	int ofs;
 
 	cg_wlock(&ch_lock);
-	cgtime(&block_timeval);
+	cgtime_real(&block_timeval);
 	strcpy(current_hash, hexstr);
 	cg_memcpy(current_block, bedata, 32);
 	get_timestamp(blocktime, sizeof(blocktime), &block_timeval);
@@ -10422,10 +10422,12 @@ begin_bench:
 		cgpu->rolling = cgpu->total_mhashes = 0;
 	}
 
+	cgtime_real(&total_tv_start);
+	get_datestamp(datestamp, sizeof(datestamp), &total_tv_start);
+
 	cgtime(&total_tv_start);
 	cgtime(&total_tv_end);
 	cgtime(&tv_hashmeter);
-	get_datestamp(datestamp, sizeof(datestamp), &total_tv_start);
 
 	watchpool_thr_id = 2;
 	thr = &control_thr[watchpool_thr_id];
